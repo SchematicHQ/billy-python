f66ddfaa1c48a9102a9c5078cebff70b876e2471
Jasdeep Garcha <jasdeepsgarcha@gmail.com>
2024-06-03 23:04:17 -0600

    add favorite logic

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import sys
 import xmltodict, json
 import config
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 
 app = Flask(__name__)
 flickr_api.set_keys(api_key = os.environ.get("FLICKR_API_KEY"), api_secret=os.environ.get("FLICKR_SECRET_KEY"))
@@ -27,9 +27,26 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-# set config variables
-#SECRET_KEY = os.urandom(32)
-#app.config['SECRET_KEY'] = SECRET_KEY
+# model classes
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    company = db.Column(db.String(250), unique=True, nullable=False)
+    users = db.relationship('Users', backref='company', lazy=True, uselist=False)
+    favorites = db.relationship('Favorites', backref='company', lazy=True)
+
+class Favorites(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(250), unique=True, nullable=False)
+    photo_id = db.Column(db.String(250), unique=True, nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+
+## routes
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -42,7 +59,7 @@ def search():
     if form.validate_on_submit():
         print("in", file=sys.stderr)
         searched_data = form.search.data
-        photo_array = search_flickr(searched_data)
+        photo_array = retrieve_images_by_keyword(searched_data)
         return render_template('search.html', 
                                form = form,
                                search=searched_data,
@@ -53,24 +70,33 @@ def search():
 def settings():
     render_template('settings.html')
 
+@app.route('/submit_favorite/<photo_id>', methods=['GET','POST'])
+def add_favorite(photo_id):
+    print("in function")
+    if request.method == 'GET': # POST request
+        user = current_user
+        company = Company.query.get(current_user.company_id)
+        favorite = Favorites(photo_id=photo_id)
+        company.favorites.append(favorite)
+
+        db.session.add(favorite)
+        db.session.commit()
+        return photo_id;
+
 @app.route('/favorites')
 def favorites():
-    render_template('favorites.html')
-
-# model classes
-class Users(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(250), unique=True,
-                         nullable=False)
-    password = db.Column(db.String(250),
-                         nullable=False)
+    user = current_user
+    company = Company.query.get(current_user.company_id)
+    favorites = company.favorites
+    photo_array = retrieve_images_by_photo_id(favorites)
+    return render_template('favorites.html', 
+                               photos = photo_array)
 
 # login, logout, registration
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    # If a post request was made, find the user by 
-    # filtering for the username
+    # If a post request was made, find the user by filtering for the username
     if request.method == "POST":
         user = Users.query.filter_by(
             username=request.form.get("username")).first()
@@ -89,27 +115,51 @@ def logout():
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
-  # If the user made a POST request, create a new user
-    if request.method == "POST":
+  # If the user made a POST request, create a new user and company
+    if request.method == "POST":        
         user = Users(username=request.form.get("username"),
                      password=request.form.get("password"))
         # Add the user to the database
+        company = Company(company=request.form.get("company"),
+                          users=user)
+
+        db.session.add(company)
         db.session.add(user)
         # Commit the changes made
         db.session.commit()
+        print(company.users.username)
         # Once user account created, redirect them to login route
         return redirect(url_for("login"))
     # Renders sign_up template if user made a GET request
     return render_template("sign_up.html")
 
 # flickr integration
-def search_flickr(search):
+def retrieve_images_by_keyword(search):
     photo_array = {}
 
     photos = xmltodict.parse(flickr.photos.search(text = search, per_page = 20, page = 1, safe_search = 1))
 
     for photo in photos['rsp']['photos']['photo']:
         photo_array[photo['@id']] = {"owner" : photo['@owner'], "id" : photo['@id'], "url" : "https://live.staticflickr.com/"+photo['@server']+"/"+photo["@id"]+"_"+photo['@secret']+".jpg"}
+    
+    print(photo_array, file=sys.stderr)
+
+    return photo_array
+
+def retrieve_images_by_photo_id(photos):
+    photo_array = {}
+    
+    print("in function")
+    print(photos)
+
+    for photo in photos:
+        print(photo)
+        print(photo.photo_id)
+        if photo.photo_id:
+            photo_metadata = xmltodict.parse(flickr.photos.getInfo(photo_id = photo.photo_id))
+            photo_metadata = photo_metadata['rsp']['photo']
+            print(photo_metadata)
+            photo_array[photo_metadata['@id']] = {"owner" : photo_metadata['owner']['@nsid'], "id" : photo_metadata['@id'], "url" : "https://live.staticflickr.com/"+photo_metadata['@server']+"/"+photo_metadata["@id"]+"_"+photo_metadata['@secret']+".jpg"}
     
     print(photo_array, file=sys.stderr)
 
